b0ecd358ac5fd6de914c7bd7e2341e8de8ca4d61
Артур Семенов <arty@S08VIDEO-08151.regions.alfaintra.net>
2022-05-31 19:30:26 +0300

    change init_db

--- src/utils.py
+++ src/utils.py
@@ -33,9 +33,9 @@ def _init_db():
 
 def check_db_exists():
     """Проверяет, инициализирована ли БД, если нет — инициализирует"""
-    cursor.execute("SELECT count(*) FROM sqlite_master WHERE type='table'")
+    cursor.execute("SELECT count(*) FROM sqlite_master WHERE name in ('stats', 'wg0_users', 'user_map', 'v_stats')")
     table_exists = cursor.fetchall()[0][0]
-    if table_exists == 3:
+    if table_exists >= 4:
         return
     _init_db()
 
